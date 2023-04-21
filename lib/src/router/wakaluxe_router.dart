66962fd08299ecc90d05ec2ff430340663244100
mbohbless <mbohblesspearl@gmail.com>
2023-04-21 20:55:52 +0100

    UPDATE: Migrated routing from autoroute 5.0.3 to autoroute 6.3.0 --Breaking changes

--- lib/src/router/wakaluxe_router.dart
+++ lib/src/router/wakaluxe_router.dart
@@ -1,22 +1,31 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:wakaluxe/src/features/onboarding/onboarding.dart';
+// @AdaptiveAutoRouter(
+//   replaceInRouteName: 'Page,Route',
+//   routes: <AutoRoute>[
 
-@AdaptiveAutoRouter(
-  replaceInRouteName: 'Page,Route',
-  routes: <AutoRoute>[
+//   ],
+// )
+
+import 'package:auto_route/auto_route.dart';
+
+import 'package:wakaluxe/src/router/wakaluxe_router.gr.dart';
+
+@AutoRouterConfig()
+class WakaluxeRouter extends $WakaluxeRouter {
+  @override
+  @override
+  final List<AutoRoute> routes = [
     AutoRoute(
-      page: Welcome,
+      page: Welcome.page,
       initial: true,
       path: '/welcome',
     ),
     AutoRoute(
-      page: ChooseLanguage,
+      page: ChooseLanguage.page,
       path: '/choose-language',
     ),
     AutoRoute(
-      page: ChooseTheme,
+      page: ChooseTheme.page,
       path: '/choose-theme',
     ),
-  ],
-)
-class $AppRouter {}
+  ];
+}
--- lib/src/router/wakaluxe_router.gr.dart
+++ lib/src/router/wakaluxe_router.gr.dart
@@ -1,101 +1,84 @@
-// **************************************************************************
-// AutoRouteGenerator
-// **************************************************************************
-
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
-// AutoRouteGenerator
+// AutoRouterGenerator
 // **************************************************************************
-//
+
 // ignore_for_file: type=lint
+// coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i2;
-import 'package:flutter/material.dart' as _i3;
-import 'package:wakaluxe/src/features/onboarding/onboarding.dart' as _i1;
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:wakaluxe/src/features/onboarding/language/choose_language.dart'
+    as _i2;
+import 'package:wakaluxe/src/features/onboarding/thememode/choose_theme.dart'
+    as _i1;
+import 'package:wakaluxe/src/features/onboarding/welcome/welcome.dart' as _i3;
 
-class AppRouter extends _i2.RootStackRouter {
-  AppRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
-      : super(navigatorKey);
+abstract class $WakaluxeRouter extends _i4.RootStackRouter {
+  $WakaluxeRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i2.PageFactory> pagesMap = {
-    Welcome.name: (routeData) {
-      return _i2.AdaptivePage<dynamic>(
-        routeData: routeData,
-        child: const _i1.Welcome(),
-      );
-    },
-    ChooseLanguage.name: (routeData) {
-      return _i2.AdaptivePage<dynamic>(
-        routeData: routeData,
-        child: const _i1.ChooseLanguage(),
-      );
-    },
+  final Map<String, _i4.PageFactory> pagesMap = {
     ChooseTheme.name: (routeData) {
-      return _i2.AdaptivePage<dynamic>(
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.ChooseTheme(),
       );
     },
+    ChooseLanguage.name: (routeData) {
+      return _i4.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i2.ChooseLanguage(),
+      );
+    },
+    Welcome.name: (routeData) {
+      return _i4.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i3.Welcome(),
+      );
+    },
   };
-
-  @override
-  List<_i2.RouteConfig> get routes => [
-        _i2.RouteConfig(
-          '/#redirect',
-          path: '/',
-          redirectTo: '/welcome',
-          fullMatch: true,
-        ),
-        _i2.RouteConfig(
-          Welcome.name,
-          path: '/welcome',
-        ),
-        _i2.RouteConfig(
-          ChooseLanguage.name,
-          path: '/choose-language',
-        ),
-        _i2.RouteConfig(
-          ChooseTheme.name,
-          path: '/choose-theme',
-        ),
-      ];
-}
-
-/// generated route for
-/// [_i1.Welcome]
-class Welcome extends _i2.PageRouteInfo<void> {
-  const Welcome()
-      : super(
-          Welcome.name,
-          path: '/welcome',
-        );
-
-  static const String name = 'Welcome';
-}
-
-/// generated route for
-/// [_i1.ChooseLanguage]
-class ChooseLanguage extends _i2.PageRouteInfo<void> {
-  const ChooseLanguage()
-      : super(
-          ChooseLanguage.name,
-          path: '/choose-language',
-        );
-
-  static const String name = 'ChooseLanguage';
 }
 
 /// generated route for
 /// [_i1.ChooseTheme]
-class ChooseTheme extends _i2.PageRouteInfo<void> {
-  const ChooseTheme()
+class ChooseTheme extends _i4.PageRouteInfo<void> {
+  const ChooseTheme({List<_i4.PageRouteInfo>? children})
       : super(
           ChooseTheme.name,
-          path: '/choose-theme',
+          initialChildren: children,
         );
 
   static const String name = 'ChooseTheme';
+
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.ChooseLanguage]
+class ChooseLanguage extends _i4.PageRouteInfo<void> {
+  const ChooseLanguage({List<_i4.PageRouteInfo>? children})
+      : super(
+          ChooseLanguage.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ChooseLanguage';
+
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i3.Welcome]
+class Welcome extends _i4.PageRouteInfo<void> {
+  const Welcome({List<_i4.PageRouteInfo>? children})
+      : super(
+          Welcome.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'Welcome';
+
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
 }
