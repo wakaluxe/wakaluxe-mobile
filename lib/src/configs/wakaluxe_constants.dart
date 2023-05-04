1af42d171068e14c3e0fd8298737c2cbe1526f8f
h.chendjou <h.chendjou@commonfactor.tech>
2023-05-04 13:01:56 +0100

    Refactored icon naming conventions and implemented screen scaling.
    This commit changes the names of several icon files to lowercase, and adds more explicit naming constants to the constants file to prevent misspelling. It also initializes screen scaling using the `flutter_screenutil` package in the app file. The dependencies in `pubspec.yaml` have been updated accordingly.

--- lib/src/configs/wakaluxe_constants.dart
+++ lib/src/configs/wakaluxe_constants.dart
@@ -5,4 +5,15 @@ class Constants {
 
   static const double borderRadius = 14;
   static const double buttonHeight = 52;
+
+  static const String closeIcon = 'assets/icons/hicon/bold/close.svg';
+  static const String locationIcon = 'assets/icons/hicon/linear/location.svg';
+  static const String settingIcon = 'assets/icons/hicon/linear/setting.svg';
+  static const String headphoneIcon = 'assets/icons/hicon/linear/headphone.svg';
+  static const String messageIcon = 'assets/icons/hicon/linear/message.svg';
+  static const String backBoldIcon = 'assets/icons/hicon/bold/back.svg';
+  static const String searchIcon = 'assets/icons/hicon/linear/search.svg';
+  static const String calendarIcon = 'assets/icons/hicon/linear/calendar.svg';
+  static const String callIcon = 'assets/icons/hicon/linear/call.svg';
+  static const String sendIcon = 'assets/icons/hicon/linear/send.svg';
 }
