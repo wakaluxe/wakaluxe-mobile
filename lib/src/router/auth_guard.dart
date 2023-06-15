343c62bd070839b171162ca8a131de9c68bb3402
baimamboukar <baimamboukar@gmail.com>
2023-06-15 08:27:36 +0100

    merge with dev

--- lib/src/router/auth_guard.dart
+++ lib/src/router/auth_guard.dart
@@ -3,16 +3,6 @@ import 'package:auto_route/auto_route.dart';
 class AuthGuard extends AutoRouteGuard {
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) {
-    // the navigation is paused until resolver.next() is called with either
-    // true to resume/continue navigation or false to abort navigation
-    if (false) {
-      // if user is authenticated we continue
-      resolver.next();
-    } else {
-      // we redirect the user to our login page
-      router.pushNamed(
-        '/welcome',
-      );
-    }
+    resolver.next();
   }
 }
